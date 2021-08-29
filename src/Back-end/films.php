4bc2d4a72ed93f0ab8bf97a3f75ee31002c51f15
Dawid Czyewski <dawidek12.04@wp.pl>
2021-08-29 17:38:43 +0200

    feat: add films.php

---
+++ src/Back-end/films.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Dawid Czyżewski">
+        <link rel="stylesheet" type="text/css" href="../Front-end/style.css">
+        <script type="text/javascript" src="../Front-end/darkMode.js"></script>
+        <title>Films</title>
+    </head>
+    <body class="notDarkModeBody" id="body">
+        <div id="Contener">
+            <div id="navBar" class="notDarkModeNavBar">
+                <ul>
+                    <li><a href="../Front-end/index.html" class="notDarkMode">home</a></li
+                    <li><a href="films.php"  class="notDarkMode">films</a></li>
+                    <li><a href=""  class="notDarkMode">author</a></li>
+                    <button type="button" id="siteColorsButton" onclick="darkMode()">dark mode</button>
+                </ul>   
+            </div>
+            <div id="footer" class="notDarkMode">
+                Project for portfolio
+            </div>
+        </div>
+    </body>
+</html>
