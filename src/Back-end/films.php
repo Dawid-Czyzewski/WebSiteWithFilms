64b7d6fbc0f75bbd940d5b7f9adf3f89533392a3
Dawid Czyewski <dawidek12.04@wp.pl>
2021-09-01 19:23:45 +0200

    feat: add path to author.html

--- src/Back-end/films.php
+++ src/Back-end/films.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Dawid Czyżewski">
         <link rel="stylesheet" type="text/css" href="../Front-end/style.css">
-        <script type="text/javascript" src="../Front-end/darkMode.js"></script>
+        <script type="text/javascript" src="../Front-end/filmsDarkMode.js"></script>
         <title>Films</title>
     </head>
     <body class="notDarkModeBody" id="body">
@@ -18,8 +18,8 @@
                 <ul>
                     <li><a href="../Front-end/index.html" class="notDarkMode">home</a></li
                     <li><a href="films.php"  class="notDarkMode">films</a></li>
-                    <li><a href=""  class="notDarkMode">author</a></li>
-                    <button type="button" id="siteColorsButton" onclick="darkMode()">dark mode</button>
+                    <li><a href="../Front-end/author.html"  class="notDarkMode">author</a></li>
+                    <button type="button" id="siteColorsButton" onclick="filmsDarkMode()">dark mode</button>
                 </ul>   
             </div>
             <div id="content">
